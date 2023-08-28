d03f8cea35fac165ac35906e4c84eccde31c9895
Oladiran Matthew Oluwatoyosi <toyoski@gmail.com>
2023-08-28 12:31:44 +0100

    Update __init__.py

--- library/__init__.py
+++ library/__init__.py
@@ -7,7 +7,7 @@ import psycopg2
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'xXe47WeadD7ihp0N3rOLSFPR8dC43mUf'
+app.config['SECRET_KEY'] = 'redwire'
 
 os.environ['SQL_DATABASE_URL'] = 'postgres://matthew:xXe47WeadD7ihp0N3rOLSFPR8dC43mUf@dpg-cjku135k5scs73d2jrhg-a.frankfurt-postgres.render.com/redwire'
 
@@ -16,7 +16,7 @@ ENV = 'dev'
 if ENV == 'dev':
     # Use the PostgreSQL URL with your credentials
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://matthew:xXe47WeadD7ihp0N3rOLSFPR8dC43mUf@dpg-cjku135k5scs73d2jrhg-a.frankfurt-postgres.render.com/redwire'
-    app.config['SECRET_KEY'] = 'xXe47WeadD7ihp0N3rOLSFPR8dC43mUf'
+    app.config['SECRET_KEY'] = 'redwire'
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQL_DATABASE_URL']  # Set this in your environment
     app.config['SECRET_KEY'] = os.environ['SECRET_KEY']  # Set this in your environment
